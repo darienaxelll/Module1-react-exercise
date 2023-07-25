b18c3793eb92a6215f445f42bfb71e66536db0d3
Darien Salazar <darien.axell.salazar@gmail.com>
2023-07-24 21:52:20 -0500

    fixed bugged related to showing concepts info through props

--- src/components/Concepts.js
+++ src/components/Concepts.js
@@ -1,9 +1,17 @@
 import "./Concepts.css";
 
 const Concepts = (props) => {
+  const componentImage = props.concepts[0].image;
+  const componentTitle = props.concepts[0].title;
+  const componentDescription = props.concepts[0].description;
+
   return (
     <ul id="concepts">
-      <ConceptItem concepts={props.concepts[0]}/>
+      <li className="concept">
+        <img src={componentImage} alt="TODO: TITLE" />
+        <h2>{componentTitle}</h2>
+        <p>{componentDescription}</p>
+      </li>
     </ul>
   );
 };
